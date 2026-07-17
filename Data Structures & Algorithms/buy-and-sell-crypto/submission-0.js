349542e034d1827aa25f961f3e098f1a529eccf1
John O.King <johnolufemikingiyanu@gmail.com>
2026-07-17 18:52:23 +0100

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {   // [7, 1, 5, 3, 6, 4]
+        let maxProfit = 0 
+
+        for(let i = 0; i < prices.length; i++) { 
+            for(let j = i + 1; j < prices.length; j++) {  
+                let profit = prices[j] - prices[i]; 
+
+                maxProfit = Math.max(maxProfit, profit); 
+            }
+        }
+
+        return maxProfit
+    }
+}
