0c2428c0145841307554f8430071f36aecd6d04f
John O.King <johnolufemikingiyanu@gmail.com>
2026-07-18 17:33:14 +0100

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(s) { 
+        let stack = [];
+        let charMap = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        };
+
+        for (let char of s) {
+            if(charMap[char]) {
+                if(stack.length > 0 && stack[stack.length - 1] === charMap[char]) {
+                    stack.pop()
+                } else {
+                    return false
+                }
+            } else {
+                stack.push(char)
+            }
+        }
+
+        return stack.length === 0
+    }
+}
