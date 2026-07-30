584b097e6c971a22ad5c34f2963e6aee34ca4fd0
John O.King <johnolufemikingiyanu@gmail.com>
2026-07-30 09:04:36 +0100

    Add: binary-tree-preorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-0.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    preorderTraversal(root) {
+        let result = [] 
+
+        if(!root) {
+            return result;
+        } 
+
+        result.push(root.val);
+
+        result.push(...this.preorderTraversal(root.left))
+        result.push(...this.preorderTraversal(root.right))
+
+        return result
+    }
+}
